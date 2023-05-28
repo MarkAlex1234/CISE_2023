739f115058ff656898afb33401e8fbf54c738841
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-05-28 17:45:56 +1200

    first commit

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aut.ac.nz"
           target="_blank"
           rel="noopener noreferrer"
         >
